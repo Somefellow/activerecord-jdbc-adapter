8d9b099996af69b4b799daef9e98e820520a66ae
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-12-19 22:46:17 +1100

    implement :if_not_exists option to create table
    - This is a workaround, SQL Server does not have
      'CREATE TABLE IF NOT EXISTS [table_name]'

--- lib/arjdbc/mssql/schema_creation.rb
+++ lib/arjdbc/mssql/schema_creation.rb
@@ -11,8 +11,28 @@ module ActiveRecord
             projections, source = query.match(%r{SELECT\s+(.*)?\s+FROM\s+(.*)?}).captures
             select_into = "SELECT #{projections} INTO #{table_name} FROM #{source}"
           else
-            o.instance_variable_set :@as, nil
-            super
+            # o.instance_variable_set :@as, nil
+            # super
+            create_sql = +''
+
+            create_sql << "IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name='#{o.name}' and xtype='U') " if o.if_not_exists
+            create_sql << "CREATE#{table_modifier_in_create(o)} TABLE "
+            create_sql << "#{quote_table_name(o.name)} "
+
+            statements = o.columns.map { |c| accept c }
+            statements << accept(o.primary_keys) if o.primary_keys
+
+            if supports_indexes_in_create?
+              statements.concat(o.indexes.map { |column_name, options| index_in_create(o.name, column_name, options) })
+            end
+
+            if supports_foreign_keys?
+              statements.concat(o.foreign_keys.map { |to_table, options| foreign_key_in_create(o.name, to_table, options) })
+            end
+
+            create_sql << "(#{statements.join(', ')})" if statements.present?
+            add_table_options!(create_sql, table_options(o))
+            create_sql
           end
         end
 

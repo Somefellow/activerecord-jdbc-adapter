b2a1830aceab64e870da0d0fc3609a76f254adbd
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-12-19 22:36:48 +1100

    JC: fix ar virtual attributes tests, adapter used to thinks they were real columns

--- lib/arjdbc/mssql/extensions/attribute_methods.rb
+++ lib/arjdbc/mssql/extensions/attribute_methods.rb
@@ -15,7 +15,9 @@ module ActiveRecord
         # this behaviour, even the current comments for that method says that
         # it rejects primary key but it doesn't (maybe a rails bug?)
         def attributes_for_update(attribute_names)
-          attribute_names.reject do |name|
+          attribute_names &= self.class.column_names
+
+          attribute_names.delete_if do |name|
             # It seems is only required to check if column in identity or not.
             # This allows to update rails custom primary keys
             next true if readonly_attribute?(name)

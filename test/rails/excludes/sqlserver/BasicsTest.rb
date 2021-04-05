9389eecdd39405062cfd8eb0d77d79ea3cc8155a
Jesse Chavez <jesse.chavez.r@gmail.com>
2021-04-05 23:23:27 +1000

    exclude some AR test from running, not applicable to jruby and/or sqlserver

--- test/rails/excludes/sqlserver/BasicsTest.rb
+++ test/rails/excludes/sqlserver/BasicsTest.rb
@@ -1,6 +1,4 @@
 # exclude some basic tests
-exclude :test_an_explain_query_does_not_raise_if_preventing_writes, 'Explain in SQL Server will produce two extra queries (e.g. SET SHOWPLAN_TEXT ON/OFF)'
-
 exclude :test_respect_internal_encoding, "missing transcoding?  Issue #883"
 
 exclude :test_column_names_are_escaped, 'for sqlserver the bad char would be [ or ]'
--- test/rails/excludes/sqlserver/EagerLoadingTooManyIdsTest.rb
+++ test/rails/excludes/sqlserver/EagerLoadingTooManyIdsTest.rb
@@ -1,3 +1,3 @@
 # I tried 1000 and still SQL Server fails to process the query
 exclude :test_preloading_too_many_ids, 'does not have a limit, mssql fails to load many record created by the fixture'
-exclude :test_eager_loading_too_may_ids, 'eager_load does not have a limit, mssql fails to load 65535 record created by the fixture'
+exclude :test_eager_loading_too_many_ids, 'eager_load does not have a limit, mssql fails to load 65535 record created by the fixture'
